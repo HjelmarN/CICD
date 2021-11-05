aeb1a147f32d535010b2eee1cb351c8ccda1ad62
Elias Hjelm <hjelmarn@protonmail.com>
2021-11-05 14:09:01 +0100

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace CICD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, good bye? Maybe not? Hello there");
+            Console.WriteLine("Test123");
         }
     }
 }
